4d32ffddbc6c5074047191a83743271256c8ae77
Gianluca Marchese <gianluca.marchese93@gmail.com>
2026-01-28 22:49:58 +0000

    Refactor partner info logic for readability

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -3,17 +3,22 @@ import React from 'react';
 export const Person = ({ person }) => {
   const { name, age, sex, isMarried, partnerName } = person;
 
+  let partnerInfo = 'I am not married';
+
+  if (isMarried) {
+    const partnerRole = sex === 'm' ? 'wife' : 'husband';
+    partnerInfo = `${partnerName} is my ${partnerRole}`;
+  }
+
   return (
     <section className="Person">
       <h2 className="Person__name">My name is {name}</h2>
 
-      {age !== undefined && <p className="Person__age">I am {age}</p>}
+      {age !== undefined && (
+        <p className="Person__age">I am {age}</p>
+      )}
 
-      <p className="Person__partner">
-        {isMarried
-          ? `${partnerName} is my ${sex === 'm' ? 'wife' : 'husband'}`
-          : 'I am not married'}
-      </p>
+      <p className="Person__partner">{partnerInfo}</p>
     </section>
   );
 };
